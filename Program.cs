7c60de66be58b0b4f76bdf457e46dd3eb392bfbe
Elias Alejandro Diaz Cahuas <N00369765@upn.pe>
2023-10-05 02:20:35 -0500

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("\t\t\tAlgoritmo para mostrar una figura en base a una variable");
+Console.WriteLine("\t\t\t*********************************************************\n");
+Console.WriteLine("Inserte una variable y luego ponga 'enter' para continuar o presione 'espacio' para cerrar el programa: \n");
+ConsoleKey tecla;
+
+do
+{
+    Console.Write("Ingrese un número: ");
+    int numero = int.Parse(Console.ReadLine()!);
+    Console.WriteLine();
+    for (int i = 1; i <= numero; i++)
+    {
+        for (int j = 1; j < numero * 2; j++)
+        {
+            Console.Write("*");
+
+        }
+        Console.WriteLine("*");
+    }
+    tecla = Console.ReadKey().Key;
+} while (tecla != ConsoleKey.Spacebar);
